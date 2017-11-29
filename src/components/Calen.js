d25735fe12b5a7b84ce8fdf6671d753aad5ac4bd
Marcelo Santiago <marcsansms@gmail.com>
2017-11-29 11:11:13 +0000

    style improvements

--- src/components/Calen.js
+++ src/components/Calen.js
@@ -1,169 +1,154 @@
 import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import momentjs from 'moment';
-import { extendMoment } from 'moment-range';
-import { DEFAULT_DATE_FORMAT } from './constants';
+import moment from 'moment';
+
 import Calendar from './Calendar';
 import CalendarNavigator from './CalendarNavigator';
 
-const moment = extendMoment(momentjs);
-
 class Calen extends PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            period: this.props.period,
-            data: this.props.data,
-            daysQuantity: this.props.daysQuantity || 7,
-        };
-        this.setDaysQuantity = this.setDaysQuantity.bind(this);
-        this.resetDaysQuantityOnResize = this.resetDaysQuantityOnResize.bind(this);
-        this.handleDayClick = this.handleDayClick.bind(this);
-        this.handlePeriodChange = this.handlePeriodChange.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      period: {
+        from: moment('2017-11-20'),
+        to: moment('2017-11-26'),
+      },
+      data: {
+        '2017-11-20': {
+          events: [],
+          tasks: [],
+        },
+        '2017-11-21': {
+          events: [],
+          tasks: [],
+        },
+        '2017-11-22': {
+          events: [
+            {
+              id: 1,
+              name: '12 Services',
+            },
+            {
+              id: 2,
+              name: '120 Updates',
+            },
+            {
+              id: 3,
+              name: '2 Reservations',
+            },
+          ],
+          tasks: [],
+          active: true,
+        },
+        '2017-11-23': {
+          events: [],
+          tasks: [],
+        },
+        '2017-11-24': {
+          events: [],
+          tasks: [],
+        },
+        '2017-11-25': {
+          events: [
+            {
+              id: 4,
+              name: '1 Update',
+            },
+            {
+              id: 5,
+              name: '3 Date confirmations',
+            },
+          ],
+          tasks: [],
+        },
+        '2017-11-26': {
+          events: [],
+          tasks: [],
+        },
+      },
+      daysQuantity: 7,
+    };
+    this.setDaysQuantity = this.setDaysQuantity.bind(this);
+    this.handleDayClick = this.handleDayClick.bind(this);
+    this.handlePeriodChange = this.handlePeriodChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.setDaysQuantity();
+  }
+
+  componentWillUnmount() {
+    // TODO: remove event listener
+  }
+
+  setDaysQuantity() {
+    const sm = window.matchMedia('(min-width: 576px)');
+    const md = window.matchMedia('(min-width: 768px)');
+    const lg = window.matchMedia('(min-width: 992px)');
+    const xl = window.matchMedia('(min-width: 1200px)');
+
+    xl.addListener(this.setDaysQuantity);
+    lg.addListener(this.setDaysQuantity);
+    md.addListener(this.setDaysQuantity);
+    sm.addListener(this.setDaysQuantity);
+
+    if (xl.matches) {
+      this.setState({ daysQuantity: 7 });
+    } else if (lg.matches) {
+      this.setState({ daysQuantity: 5 });
+    } else if (md.matches) {
+      this.setState({ daysQuantity: 4 });
+    } else if (sm.matches) {
+      this.setState({ daysQuantity: 2 });
+    } else {
+      this.setState({ daysQuantity: 1 });
     }
+  }
 
-    componentDidMount() {
-        if(!this.props.daysQuantity) {
-            this.handleWindowResize();
-        }
-        this.setActiveDay(moment().format(DEFAULT_DATE_FORMAT));
+
+  setActiveDay(day) {
+    const data = this.resetActiveDay();
+    const date = day.date;
+    if (!data.hasOwnProperty(day.date)) {
+      data[date] = { events: [], actions: [] };
     }
+    data[date].active = true;
+    this.setState({ data });
+  }
 
-    componentWillUnmount() {
-        // TODO: remove event listener
-    }
+  resetActiveDay() {
+    const data = this.state.data;
+    return Object.keys(data).reduce((obj, date) => {
+      data[date].active = false;
+      obj[date] = data[date];
+      return obj;
+    }, {});
+  }
+  handleDayClick(day) {
+    this.setActiveDay(day);
+  }
 
-    setDaysQuantity(quantity) {
-        this.setState({ daysQuantity: quantity });
+  handlePeriodChange(period) {
+    const date = period.from.format('YYYY-MM-DD');
+    this.setActiveDay({ date });
+    this.setState({ period });
+  }
 
-        const period = {
-            from: moment().startOf('week'),
-            to: moment().startOf('week').add(quantity - 1, 'days')
-        };
-
-        if (quantity < 7) {
-            period.from = moment();
-            period.to = moment().add(quantity - 1, 'days');
-        }
-
-        const onDaysQuantityChange = this.props.onDaysQuantityChange;
-        if (onDaysQuantityChange) {
-            onDaysQuantityChange(quantity);
-        }
-
-        this.handlePeriodChange(period);
-    }
-
-    setActiveDay(day) {
-        const data = this.resetActiveDay();
-        if (!data.hasOwnProperty(day)) {
-            data[day] = { events: [], actions: [] };
-        }
-        data[day].active = true;
-        this.setState({ data });
-    }
-    
-    resetActiveDay() {
-        const data = this.state.data;
-        return Object.keys(data)
-            .reduce((obj, date) => {
-                data[date].active = false;
-                obj[date] = data[date];
-                return obj;
-            }, {});
-    }
-
-    resetDaysQuantityOnResize() {
-        const sm = window.matchMedia("(min-width: 576px)");
-        const md = window.matchMedia("(min-width: 768px)");
-        const lg = window.matchMedia("(min-width: 992px)");
-        const xl = window.matchMedia("(min-width: 1200px)");
-        
-        xl.addListener(this.resetDaysQuantityOnResize);
-        lg.addListener(this.resetDaysQuantityOnResize);
-        md.addListener(this.resetDaysQuantityOnResize);
-        sm.addListener(this.resetDaysQuantityOnResize);
-        
-        let quantity = this.state.daysQuantity;
-        if (xl.matches) {
-            quantity = 7;
-        } else if (lg.matches) {
-            quantity = 5;
-        } else if (md.matches) {
-            quantity = 4;
-        } else if (sm.matches) {
-            quantity = 2;
-        } else {
-            quantity = 1;
-        }
-        this.setDaysQuantity(quantity);
-    }
-
-    handleWindowResize() {
-        this.resetDaysQuantityOnResize();
-    }
-
-    handleDayClick(day) {
-        this.setActiveDay(day);
-    }
-
-    handlePeriodChange(period) {
-        const today = moment();
-        const range = moment.range(period.from, period.to);
-        let date = period.from.format(DEFAULT_DATE_FORMAT);
-        if (range.contains(today)) {
-            date = today.format(DEFAULT_DATE_FORMAT);
-        }
-        this.setActiveDay(date);
-        this.setState({ period });
-
-        const onPeriodChange = this.props.onPeriodChange;
-        if (onPeriodChange) {
-            onPeriodChange(period);
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <CalendarNavigator
-                    period={this.state.period}
-                    daysQuantity={this.state.daysQuantity}
-                    onPeriodChange={this.handlePeriodChange}
-                />
-                <Calendar
-                    period={this.state.period}
-                    data={this.state.data}
-                    onDayClick={this.handleDayClick}
-                />
-            </div>
-        );
-    }
-}
-
-Calen.defaultProps = {
-    period: {
-        from: moment().startOf('week'),
-        to: moment().startOf('week').add(6, 'days')
-    },
-    data: {},
-}
-
-Calen.propTypes = {
-    period: PropTypes.shape({
-        from: PropTypes.oneOfType([
-            PropTypes.instanceOf(Date),
-            PropTypes.instanceOf(moment),
-        ]),
-        to: PropTypes.oneOfType([
-            PropTypes.instanceOf(Date),
-            PropTypes.instanceOf(moment),
-        ]),
-    }),
-    data: PropTypes.object,
-    daysQuantity: PropTypes.number,
-    onPeriodChange: PropTypes.func,
-    onDaysQuantityChange: PropTypes.func,
+  render() {
+    return (
+      <div>
+        <CalendarNavigator
+          period={this.state.period}
+          daysQuantity={this.state.daysQuantity}
+          onPeriodChange={this.handlePeriodChange}
+        />
+        <Calendar
+          period={this.state.period}
+          data={this.state.data}
+          onDayClick={this.handleDayClick}
+        />
+      </div>
+    );
+  }
 }
 
 export default Calen;
--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,13 +1,12 @@
 import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import momentjs from 'moment';
 import { extendMoment } from 'moment-range';
-import { DEFAULT_DATE_FORMAT } from './constants';
 import Day from './Day';
 
 const moment = extendMoment(momentjs);
 const CalendarStyled = styled.div`
+    margin: 10px;
     ul.calen-list {
         display: flex;
         flex-wrap: wrap;
@@ -15,6 +14,7 @@ const CalendarStyled = styled.div`
         width: 100%;
     }
     li.calen-list-item {
+        margin: 4px;
         flex: 1;
         display:flex;
         width:100%;
@@ -22,59 +22,44 @@ const CalendarStyled = styled.div`
 `;
 
 class Calendar extends PureComponent {
-    buildCalendar() {
-        const { from, to } = this.props.period;
-        const data = this.props.data;
-        const range = moment.range(from, to);
-        const days = Array.from(range.by('day'));
-        return days.map(day => {
-            let obj = {
-                events: [],
-                actions: [],
-                active: false,
-            };
-            const date = day.format(DEFAULT_DATE_FORMAT);
-            if(data.hasOwnProperty(date)) {
-                obj = data[date];
-            };
-            obj.date = date;
-            return obj;
-        });
-    }
+  buildCalendar() {
+    const { from, to } = this.props.period;
+    const data = this.props.data;
+    const range = moment.range(from, to);
+    const days = Array.from(range.by('day'));
+    return days.map((day) => {
+      let obj = {
+        events: [],
+        actions: [],
+        active: false,
+      };
+      const date = day.format('YYYY-MM-DD');
+      if (data.hasOwnProperty(date)) {
+        obj = data[date];
+      }
+      obj.date = date;
+      return obj;
+    });
+  }
 
-    render() {
-        const calendar = this.buildCalendar();
-        return (
-            <CalendarStyled>
-                <ul className="calen-list">
-                    {calendar.map(day => (
-                        <li
-                            className="calen-list-item"
-                            key={day.date}
-                            onClick={() => this.props.onDayClick(day.date)}
-                        >
-                            <Day {...day} />
-                        </li>
-                    ))}
-                </ul>
-            </CalendarStyled>
-        );
-    }
-}
-
-Calendar.propTypes = {
-    period: PropTypes.shape({
-        from: PropTypes.oneOfType([
-            PropTypes.instanceOf(Date),
-            PropTypes.instanceOf(moment),
-        ]),
-        to: PropTypes.oneOfType([
-            PropTypes.instanceOf(Date),
-            PropTypes.instanceOf(moment),
-        ]),
-    }).isRequired,
-    data: PropTypes.object.isRequired,
-    onDayClick: PropTypes.func.isRequired,
+  render() {
+    const calendar = this.buildCalendar();
+    return (
+      <CalendarStyled>
+        <ul className="calen-list">
+          {calendar.map(day => (
+            <li
+              className="calen-list-item"
+              key={day.date}
+              onClick={() => this.props.onDayClick(day)}
+            >
+              <Day {...day} />
+            </li>
+          ))}
+        </ul>
+      </CalendarStyled>
+    );
+  }
 }
 
 export default Calendar;
